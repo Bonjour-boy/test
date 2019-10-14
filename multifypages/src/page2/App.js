0b81887989c108cbf95d2fd4f47c0782a5c4901a
a <a@adeMacBook-Pro.local>
2019-10-14 21:51:53 +0800

    多页面应用

---
+++ multifypages/src/page2/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+export default class App2 extends Component{
+
+    render(){
+        return(
+            <div>
+                详情页
+            </div>
+        )
+    }
+}
